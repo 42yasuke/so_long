cb48cf0d74d4269fd42922a5344b72ffedc22b9f
42yasuke <j.ralph1998@gmail.com>
2023-02-23 17:39:22 +0100

    debut

--- includes/so_long.h
+++ includes/so_long.h
@@ -14,14 +14,17 @@
 # define SO_LONG_H
 
 # include "../mlx42/mlx.h"
+# include "../mlx42/mlx_int.h"
 # include "../libft/libft.h"
 
 # include <stdlib.h>
 # include <errno.h>
 # include <string.h>
 
-# define WINDOW_WIDTH 640
-# define WINDOW_HEIGHT 480
+# define WINDOW_WIDTH 1431
+# define WINDOW_HEIGHT 780
+
+# define KEY_ESC 65307
 
 # define BAD_INIT 1
 # define MALLOC_FAILED 2
@@ -36,6 +39,12 @@ typedef struct	s_image
 	int		width;
 	int		height;
 }	t_image;
+
+typedef struct	s_win
+{
+	void	*mlx;
+	void	*mlx_win;
+}	t_win;
 
 /*	error.c	*/
 void	ft_error(int err, char *err_msg);
--- src/main.c
+++ src/main.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jralph <jralph@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 18:17:16 by jose              #+#    #+#             */
-/*   Updated: 2023/02/17 14:50:17 by jralph           ###   ########.fr       */
+/*   Updated: 2023/02/23 17:30:04 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	handle_no_event(void *img_space)
+{
+	/* This function needs to exist, but it is useless for the moment */
+	return ((void)img_space, 0);
+}
 
 void	ft_img_mlx_put_pixel(t_image *img, int x, int y, int color)
 {
@@ -20,20 +26,28 @@ void	ft_img_mlx_put_pixel(t_image *img, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+int	ft_close_win(int keycode, t_win *window)
+{
+	if (keycode == KEY_ESC)
+		mlx_destroy_window(window->mlx, window->mlx_win);
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_image	img;
+	t_win	win;
+	t_image	img_space;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "SO_LONG");
-	//img.width = WINDOW_WIDTH;
-	//img.height = WINDOW_HEIGHT;
-	img.img = mlx_xpm_file_to_image(mlx, "/home/jose/C/so_long/image/eau.xpm", &img.width, &img.height);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.size_line, &img.endian);
-	 mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
-	free(mlx);
+	win.mlx = mlx_init();
+	win.mlx_win = mlx_new_window(win.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "SO_LONG");
+	img_space.img = mlx_xpm_file_to_image(win.mlx, "/home/jose/C/so_long/image/space.xpm", &img_space.width, &img_space.height);
+	img_space.addr = mlx_get_data_addr(img_space.img, &img_space.bpp, &img_space.size_line, &img_space.endian);
+	mlx_put_image_to_window(win.mlx, win.mlx_win, img_space.img, 0, 0);
+	mlx_loop_hook(win.mlx, &handle_no_event, &img_space);
+	mlx_hook(win.mlx_win, 2, 1L<<0, &ft_close_win, &win);
+	mlx_loop(win.mlx);
+	mlx_destroy_image(win.mlx, img_space.img);
+	mlx_destroy_display(win.mlx);
+	free(win.mlx);
 	return (0);
 }
