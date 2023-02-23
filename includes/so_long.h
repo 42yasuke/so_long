bedf6c791f9bd7a575496c8d97bccbd14e3e00fb
42yasuke <j.ralph1998@gmail.com>
2023-02-23 18:19:43 +0100

    on s'amuse comme des foufous

--- includes/so_long.h
+++ includes/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jralph <jralph@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 18:24:25 by jose              #+#    #+#             */
-/*   Updated: 2023/02/17 14:59:35 by jralph           ###   ########.fr       */
+/*   Updated: 2023/02/23 18:18:40 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,24 @@
 # include <errno.h>
 # include <string.h>
 
+/*	window's macro	*/
 # define WINDOW_WIDTH 1431
 # define WINDOW_HEIGHT 780
 
+/*	keybord's macro	*/
 # define KEY_ESC 65307
 
+/*	error's macro*/
 # define BAD_INIT 1
 # define MALLOC_FAILED 2
+
+/*	image's macro	*/
+# define BACKGROUND 0
+# define SPACIALSHIP 1
+# define ENEMY 2
+# define EXIT 3
+# define OBSTACLE 4
+# define ITEM 5
 
 typedef struct	s_image
 {
